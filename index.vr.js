a621446bcadea1769edcbd50f564327885f753c4
Yenly Ma <yencodes@gmail.com>
2017-04-19 07:26:50 -0700

    Install simple hello world react vr from F8 workshop.

---
+++ index.vr.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {
+  AppRegistry,
+  asset,
+  StyleSheet,
+  Pano,
+  Text,
+  View,
+} from 'react-vr';
+
+export default class f8_workshop extends React.Component {
+  render() {
+    return (
+      <View>
+        <Pano source={asset('chess-world.jpg')}/>
+        <Text
+          style={{
+            backgroundColor: '#777879',
+            fontSize: 0.8,
+            fontWeight: '400',
+            layoutOrigin: [0.5, 0.5],
+            paddingLeft: 0.2,
+            paddingRight: 0.2,
+            textAlign: 'center',
+            textAlignVertical: 'center',
+            transform: [{translate: [0, 0, -3]}],
+          }}>
+          {/* http://localhost:8081/vr/?hotreload=1 */}
+          hello f8!
+        </Text>
+      </View>
+    );
+  }
+};
+
+AppRegistry.registerComponent('f8_workshop', () => f8_workshop);
